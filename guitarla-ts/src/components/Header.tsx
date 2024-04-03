56685f2780465438274f2d14160ecf049859c876
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-04-03 16:00:08 -0500

    Fourth Commit

--- guitarla-ts/src/components/Header.tsx
+++ guitarla-ts/src/components/Header.tsx
@@ -1,8 +1,18 @@
+import type { CartItem, Guitar } from "../types";
+
+type HeaderProps = {
+	cart: CartItem[];
+	removeFromCart: (id: Guitar['id']) => void;
+	decreaseQuantity: (id: Guitar['id']) => void;
+	increaseQuantity: (id: Guitar['id']) => void;
+	clearCart: () => void;
+	isEmpty: boolean;
+	cartTotal: number;
+}
 
 
 
-
-const Header = ({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal }) => {
+const Header = ({ cart, removeFromCart, increaseQuantity, decreaseQuantity, clearCart, isEmpty, cartTotal } : HeaderProps) => {
 
 	//State Derivado
 
