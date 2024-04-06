f1275895718ae580310e181cebfb7753da975cc6
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-04-06 14:12:30 -0500

    seventh commitment

--- calorie-tracker/src/components/Form.tsx
+++ calorie-tracker/src/components/Form.tsx
@@ -1,20 +1,32 @@
-import { useState, ChangeEvent, FormEvent, Dispatch } from "react"
+import { useState, ChangeEvent, FormEvent, Dispatch, useEffect } from "react"
+import {v4 as uuidv4 } from "uuid";
 import type { Activity } from "../types";
 import { categories } from "../data/categories";
-import { ActivityActions } from "../reducers/activity-reducer";
+import { ActivityActions, ActivityState } from "../reducers/activity-reducer";
 
 type FormProps = {
-	dispatch: Dispatch<ActivityActions>
+	dispatch: Dispatch<ActivityActions>,
+	state: ActivityState
 }
 
-export default function Form({dispatch}: FormProps) {
-
-	const [activity, setActivity] = useState<Activity>({
+const initialState : Activity = {
+		id: uuidv4(),
 		category: 1,
 		name: '',
 		calories: 0
+}
 
-	});
+export default function Form({dispatch, state}: FormProps) {
+
+	const [activity, setActivity] = useState<Activity>(initialState);
+
+	useEffect(() => {
+		if(state.activeId) {
+			const selectedActivity = state.activities.filter( stateActivity => stateActivity.id === state.activeId)[0]
+			setActivity(selectedActivity)
+			
+		}
+	}, [state.activeId])
 
 	
 	const handleChange = (e: ChangeEvent<HTMLSelectElement> | ChangeEvent<HTMLInputElement>) => {
@@ -35,6 +47,10 @@ export default function Form({dispatch}: FormProps) {
 	const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
 		e.preventDefault();
 		dispatch({ type: "save-activity", payload: {newActivity: activity}})
+		setActivity({
+			...initialState,
+			id: uuidv4()
+		})
 		
 	}
 
