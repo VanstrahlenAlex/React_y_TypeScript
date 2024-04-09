eeae6ea470b5884f0f558ec71c6c61f10dda03ce
Alexander Van strahlen <vanstrahlenalex@gmail.com>
2024-04-09 15:27:52 -0500

    Finalizando contador de calorias

--- calorie-tracker/src/App.tsx
+++ calorie-tracker/src/App.tsx
@@ -1,21 +1,33 @@
 import Form from "./components/Form"
-import { useReducer } from "react";
+import { useReducer, useEffect, useMemo } from "react";
 import { activityReducer, initialState } from "./reducers/activity-reducer";
 import ActivityList from "./components/ActivityList";
+import CalorieTracker from "./components/CalorieTracker";
 
 function App() {
 
-	const [state, dispatch] = useReducer(activityReducer, initialState)
+	const [state, dispatch] = useReducer(activityReducer, initialState);
 
+	//LocalStorage
+	useEffect(() => {
+		localStorage.setItem('activities', JSON.stringify(state.activities))
+	}, [state.activities])
 
+	const canRestarApp = useMemo(() => state.activities.length , [state.activities]);
 	
 	return (
 		<>
 			<header className="bg-lime-600 py-3">
-				<div className="max-w-4xl mx-auto flex justify-between">
+				<div className="max-w-4xl mx-auto flex justify-between items-center">
 					<h1 className="text-center text-lg font-bold text-white uppercase">
 						Contador de calorias
 					</h1>
+					<button className="bg-gray-800 hover:bg-gray-900 p-2 font-bold uppercase text-white cursor-pointer rounded-md text-sm disabled:opacity-10"
+						disabled={!canRestarApp}
+						onClick={() =>dispatch({type: "restart-app"})}
+					>
+						Reiniciar App
+					</button>
 				</div>
 			</header>
 
@@ -24,6 +36,14 @@ function App() {
 					<Form 
 						dispatch={dispatch}
 						state={state}
+					/>
+				</div>
+			</section>
+
+			<section className="bg-gray-800 py-10">
+				<div className="max-w-4xl mx-auto" >
+					<CalorieTracker 
+						activities={state.activities}
 					/>
 				</div>
 			</section>
